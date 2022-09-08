ae81f69c8b2d691729b3d20254db7da1f3dbcdfa
chadokada <chadokada@gmail.com>
2022-09-07 22:23:05 -1000

    ProductCard tested sufficiently

---
+++ src/Tests/ProductCard.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import ProductCard from '../Components/ProductCard';
+
+describe("Test ProductCard", () => {
+  const testProduct = {
+    name: 'Bowler Hat',
+    type: 'Hat',
+    price: 15,
+    source: 'bowler.jpg'
+  };
+  const onClickMock = jest.fn();
+
+  it("renders price with two decimals", () => {
+    render(<ProductCard product={testProduct} handleAddToCart={onClickMock} />);
+
+    expect(
+      screen.getByText(
+        (content, element) => content.startsWith('$')).textContent)
+        .toMatch('$15.00');
+  });
+
+  it("add to cart callback called on correct number of times", () => {
+    render(<ProductCard product={testProduct} handleAddToCart={onClickMock} />);
+    const addToCart = screen.getByRole('button', {name: 'Add To Cart'});
+
+    userEvent.click(addToCart);
+    
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  })
+
+})
