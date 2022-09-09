7bbee3cb84ca492415e22c65d7edad226d3db172
chadokada <chadokada@gmail.com>
2022-09-08 22:28:22 -1000

    Deleted unnecessary comments

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,9 +5,6 @@ import  { Link } from 'react-router-dom';
 const Nav = ({shopName}) => {
   return (
     <nav className='nav'>
-      {/* 
-      <div className='store-name'>Butter and Sugar</div>
-      */}
       <h2>{shopName}</h2>
       <ul>
           <Link to="/products/all">
@@ -19,15 +16,6 @@ const Nav = ({shopName}) => {
           <Link to="/products/bags">
             <li>Bags and Totes</li>
           </Link>
-
-          {/* 
-          <li>Clothing</li>
-        
-          <li>Food Toys</li>
-        
-          <li>Miscellaneous</li>
-          */}
-
       </ul>
     </nav>
   )
