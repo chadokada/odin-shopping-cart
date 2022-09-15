73764653a031a25cbdb240ec8177e2357a857d24
chadokada <chadokada@gmail.com>
2022-09-14 21:49:09 -1000

    removed renderer import

--- src/Tests/CartBar.test.js
+++ src/Tests/CartBar.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import renderer from 'react-test-renderer'
 import userEvent from '@testing-library/user-event';
 import CartBar from '../Components/CartBar';
 
